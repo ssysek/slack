ab4dcda4167d425ba856de9b3568ac0e7a74b62c
Marek <marek2105@vp.pl>
2020-05-27 01:28:18 +0200

    final version of contactpage

--- uis/contactpage.py
+++ uis/contactpage.py
@@ -8,6 +8,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from uis.resources.stylesheets import *
 
 
 class Ui_ContactWindow(object):
@@ -17,7 +18,7 @@ class Ui_ContactWindow(object):
     def setupUi(self, MainWindow):
         self.window = MainWindow
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1009, 647)
+        MainWindow.resize(1200, 600)
         MainWindow.setMinimumSize(QtCore.QSize(620, 500))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
@@ -34,44 +35,38 @@ class Ui_ContactWindow(object):
         self.gridLayout_7.addItem(spacerItem1, 6, 1, 1, 1)
         self.horizontalLayout_6 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_6.setObjectName("horizontalLayout_6")
-        self.example_button_blue_7 = QtWidgets.QPushButton(self.centralwidget)
-        self.example_button_blue_7.setEnabled(True)
-        self.example_button_blue_7.setMinimumSize(QtCore.QSize(50, 50))
-        self.example_button_blue_7.setFocusPolicy(QtCore.Qt.StrongFocus)
-        self.example_button_blue_7.setStyleSheet("background-color:rgb(1, 107, 229);\n"
-"color:white;\n"
-"font: 75 8pt \"SansSerif\";\n"
-"border-style:outset;\n"
-"border-width:2px;\n"
-"border-radius:10px;\n"
-"border-color:white;")
-        self.example_button_blue_7.setAutoDefault(False)
-        self.example_button_blue_7.setDefault(False)
-        self.example_button_blue_7.setFlat(False)
-        self.example_button_blue_7.setObjectName("example_button_blue_7")
-        self.horizontalLayout_6.addWidget(self.example_button_blue_7)
-        self.example_button_blue_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.example_button_blue_4.setEnabled(True)
+        self.button_submit = QtWidgets.QPushButton(self.centralwidget)
+        self.button_submit.setEnabled(True)
+        self.button_submit.setMinimumSize(QtCore.QSize(50, 50))
+        self.button_submit.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.button_submit.setStyleSheet(button_for_logging_style_sheet)
+        self.button_submit.setAutoDefault(False)
+        self.button_submit.setDefault(False)
+        self.button_submit.setFlat(False)
+        self.button_submit.setObjectName("button_submit")
+        self.horizontalLayout_6.addWidget(self.button_submit)
+        self.button_cancel = QtWidgets.QPushButton(self.centralwidget)
+        self.button_cancel.setEnabled(True)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.example_button_blue_4.sizePolicy().hasHeightForWidth())
-        self.example_button_blue_4.setSizePolicy(sizePolicy)
-        self.example_button_blue_4.setMinimumSize(QtCore.QSize(50, 50))
-        self.example_button_blue_4.setFocusPolicy(QtCore.Qt.StrongFocus)
-        self.example_button_blue_4.setToolTipDuration(-1)
-        self.example_button_blue_4.setStyleSheet("background-color:    rgb(200, 200, 200);\n"
+        sizePolicy.setHeightForWidth(self.button_cancel.sizePolicy().hasHeightForWidth())
+        self.button_cancel.setSizePolicy(sizePolicy)
+        self.button_cancel.setMinimumSize(QtCore.QSize(50, 50))
+        self.button_cancel.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.button_cancel.setToolTipDuration(-1)
+        self.button_cancel.setStyleSheet("background-color:    rgb(200, 200, 200);\n"
 "color:white;\n"
-"font: 75 8pt \"SansSerif\";\n"
+"font: 450 13pt \"SansSerif\";\n"
 "border-style:outset;\n"
 "border-width:2px;\n"
-"border-radius:10px;\n"
+"border-radius:19px;\n"
 "border-color:white;")
-        self.example_button_blue_4.setAutoDefault(False)
-        self.example_button_blue_4.setDefault(False)
-        self.example_button_blue_4.setFlat(False)
-        self.example_button_blue_4.setObjectName("example_button_blue_4")
-        self.horizontalLayout_6.addWidget(self.example_button_blue_4)
+        self.button_cancel.setAutoDefault(False)
+        self.button_cancel.setDefault(False)
+        self.button_cancel.setFlat(False)
+        self.button_cancel.setObjectName("button_cancel")
+        self.horizontalLayout_6.addWidget(self.button_cancel)
         self.gridLayout_7.addLayout(self.horizontalLayout_6, 10, 1, 1, 1)
         spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
         self.gridLayout_7.addItem(spacerItem2, 2, 1, 1, 1)
@@ -79,6 +74,14 @@ class Ui_ContactWindow(object):
         self.gridLayout_7.addItem(spacerItem3, 3, 0, 1, 1)
         spacerItem4 = QtWidgets.QSpacerItem(20, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_7.addItem(spacerItem4, 3, 2, 1, 1)
+        self.button_return = QtWidgets.QPushButton(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.button_return.sizePolicy().hasHeightForWidth())
+        self.button_return.setSizePolicy(sizePolicy)
+        self.button_return.setObjectName("button_return")
+        self.gridLayout_7.addWidget(self.button_return, 0, 0, 1, 1)
         self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(255)
@@ -89,14 +92,13 @@ class Ui_ContactWindow(object):
         self.textEdit_2.setMaximumSize(QtCore.QSize(16777215, 30))
         self.textEdit_2.setBaseSize(QtCore.QSize(0, 0))
         self.textEdit_2.setFocusPolicy(QtCore.Qt.StrongFocus)
-        self.textEdit_2.setStyleSheet("background-color:     rgb(225, 225, 225);\n"
-"font: 75 8pt \"SansSerif\";\n"
-"border-color: rgb(225, 225, 225);\n"
-"border-style:solid;\n"
-"border-width:1px;\n"
-"border-radius:5px;\n"
-"\n"
-"")
+        self.textEdit_2.setStyleSheet("background-color:rgb(221, 222, 235);\n"
+                                      "color:black;\n"
+                                      "font: 75 8pt \"SansSerif\";\n"
+                                      "border-style:solid;\n"
+                                      "border-width:2px;\n"
+                                      "border-radius:11px;\n"
+                                      "border-color:rgb(212, 174, 252);")
         self.textEdit_2.setTabChangesFocus(True)
         self.textEdit_2.setObjectName("textEdit_2")
         self.gridLayout_7.addWidget(self.textEdit_2, 5, 1, 1, 1)
@@ -112,14 +114,13 @@ class Ui_ContactWindow(object):
         self.textEdit_4.setFocusPolicy(QtCore.Qt.StrongFocus)
         self.textEdit_4.setToolTip("")
         self.textEdit_4.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.textEdit_4.setStyleSheet("background-color:     rgb(225, 225, 225);\n"
-"font: 75 8pt \"SansSerif\";\n"
-"border-color: rgb(225, 225, 225);\n"
-"border-style:solid;\n"
-"border-width:1px;\n"
-"border-radius:5px;\n"
-"\n"
-"")
+        self.textEdit_4.setStyleSheet("background-color:rgb(221, 222, 235);\n"
+                                      "color:black;\n"
+                                      "font: 75 8pt \"SansSerif\";\n"
+                                      "border-style:solid;\n"
+                                      "border-width:2px;\n"
+                                      "border-radius:11px;\n"
+                                      "border-color:rgb(212, 174, 252);")
         self.textEdit_4.setTabChangesFocus(True)
         self.textEdit_4.setObjectName("textEdit_4")
         self.gridLayout_7.addWidget(self.textEdit_4, 3, 3, 1, 1)
@@ -132,14 +133,13 @@ class Ui_ContactWindow(object):
         self.textEdit_5.setMinimumSize(QtCore.QSize(0, 0))
         self.textEdit_5.setMaximumSize(QtCore.QSize(16777215, 30))
         self.textEdit_5.setFocusPolicy(QtCore.Qt.StrongFocus)
-        self.textEdit_5.setStyleSheet("background-color:     rgb(225, 225, 225);\n"
-"font: 75 8pt \"SansSerif\";\n"
-"border-color: rgb(225, 225, 225);\n"
-"border-style:solid;\n"
-"border-width:1px;\n"
-"border-radius:5px;\n"
-"\n"
-"")
+        self.textEdit_5.setStyleSheet("background-color:rgb(221, 222, 235);\n"
+                                      "color:black;\n"
+                                      "font: 75 8pt \"SansSerif\";\n"
+                                      "border-style:solid;\n"
+                                      "border-width:2px;\n"
+                                      "border-radius:11px;\n"
+                                      "border-color:rgb(212, 174, 252);")
         self.textEdit_5.setTabChangesFocus(True)
         self.textEdit_5.setObjectName("textEdit_5")
         self.gridLayout_7.addWidget(self.textEdit_5, 3, 1, 1, 1)
@@ -147,14 +147,13 @@ class Ui_ContactWindow(object):
         self.textEdit_6.setEnabled(True)
         self.textEdit_6.setFocusPolicy(QtCore.Qt.StrongFocus)
         self.textEdit_6.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.textEdit_6.setStyleSheet("background-color:     rgb(225, 225, 225);\n"
-"font: 75 8pt \"SansSerif\";\n"
-"border-color: rgb(225, 225, 225);\n"
-"border-style:solid;\n"
-"border-width:1px;\n"
-"border-radius:5px;\n"
-"\n"
-"")
+        self.textEdit_6.setStyleSheet("background-color:rgb(221, 222, 235);\n"
+                                      "color:black;\n"
+                                      "font: 75 8pt \"SansSerif\";\n"
+                                      "border-style:solid;\n"
+                                      "border-width:2px;\n"
+                                      "border-radius:11px;\n"
+                                      "border-color:rgb(212, 174, 252);")
         self.textEdit_6.setTabChangesFocus(True)
         self.textEdit_6.setObjectName("textEdit_6")
         self.gridLayout_7.addWidget(self.textEdit_6, 8, 1, 1, 3)
@@ -165,7 +164,9 @@ class Ui_ContactWindow(object):
         font.setPointSize(17)
         font.setKerning(False)
         self.label.setFont(font)
+        self.label.setStyleSheet(pretty_big_darker_label_style_sheet)
         self.label.setObjectName("label")
+
         self.gridLayout_7.addWidget(self.label, 1, 1, 1, 1)
         self.textEdit_3 = QtWidgets.QTextEdit(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.MinimumExpanding)
@@ -177,14 +178,13 @@ class Ui_ContactWindow(object):
         self.textEdit_3.setMaximumSize(QtCore.QSize(16777215, 30))
         self.textEdit_3.setBaseSize(QtCore.QSize(0, 0))
         self.textEdit_3.setFocusPolicy(QtCore.Qt.StrongFocus)
-        self.textEdit_3.setStyleSheet("background-color:     rgb(225, 225, 225);\n"
-"font: 75 8pt \"SansSerif\";\n"
-"border-color: rgb(225, 225, 225);\n"
-"border-style:solid;\n"
-"border-width:1px;\n"
-"border-radius:5px;\n"
-"\n"
-"")
+        self.textEdit_3.setStyleSheet("background-color:rgb(221, 222, 235);\n"
+                                      "color:black;\n"
+                                      "font: 75 8pt \"SansSerif\";\n"
+                                      "border-style:solid;\n"
+                                      "border-width:2px;\n"
+                                      "border-radius:11px;\n"
+                                      "border-color:rgb(212, 174, 252);")
         self.textEdit_3.setTabChangesFocus(True)
         self.textEdit_3.setObjectName("textEdit_3")
         self.gridLayout_7.addWidget(self.textEdit_3, 5, 3, 1, 1)
@@ -204,6 +204,16 @@ class Ui_ContactWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        MainWindow.setStyleSheet(gradient_style_sheet)
+
+        MainWindow.setWindowIcon(QtGui.QIcon('resources/taco.png'))
+
+        self.return_pixmap = QtGui.QPixmap("resources/return.png")
+        self.return_pixmap = self.return_pixmap.scaled(QtCore.QSize(32, 32))
+        self.icon = QtGui.QIcon(self.return_pixmap)
+        self.button_return.setIcon(self.icon)
+        self.button_return.setIconSize(QtCore.QSize(32, 32))
+
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -211,18 +221,21 @@ class Ui_ContactWindow(object):
         MainWindow.setTabOrder(self.textEdit_4, self.textEdit_2)
         MainWindow.setTabOrder(self.textEdit_2, self.textEdit_3)
         MainWindow.setTabOrder(self.textEdit_3, self.textEdit_6)
-        MainWindow.setTabOrder(self.textEdit_6, self.example_button_blue_7)
-        MainWindow.setTabOrder(self.example_button_blue_7, self.example_button_blue_4)
+        MainWindow.setTabOrder(self.textEdit_6, self.button_submit)
+        MainWindow.setTabOrder(self.button_submit, self.button_cancel)
+
 
         #tutaj logika
-        self.example_button_blue_4.clicked.connect(self.clicked_cancel)
+        self.button_cancel.clicked.connect(self.clicked_cancel)
+        self.button_return.clicked.connect(self.clicked_return)
+
 
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.example_button_blue_7.setText(_translate("MainWindow", "Submit"))
-        self.example_button_blue_4.setText(_translate("MainWindow", "Cancel"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Contact"))
+        self.button_submit.setText(_translate("MainWindow", "Submit"))
+        self.button_cancel.setText(_translate("MainWindow", "Cancel"))
         self.textEdit_2.setPlaceholderText(_translate("MainWindow", "Email"))
         self.textEdit_4.setPlaceholderText(_translate("MainWindow", "Last Name"))
         self.textEdit_5.setPlaceholderText(_translate("MainWindow", "First Name"))
@@ -233,6 +246,10 @@ class Ui_ContactWindow(object):
     def clicked_cancel(self):
         self.window.hide()
         print("cancel")
+
+    def clicked_return(self):
+        self.parent.window.show()
+        self.window.hide()
 
 
 if __name__ == "__main__":
